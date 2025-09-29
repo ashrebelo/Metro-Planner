cc2c1b712c3d5673fa3e35e9ffad3e82b3542a05
ashre <ashrebelo21@gmail.com>
2025-09-29 12:08:51 -0400

    initial commit

---
+++ metro-client/src/components/Planner.jsx
@@ -0,0 +1,11 @@
+
+function Planner() {
+  return (
+    <section id="trip-planner">
+      <h1>Metro Trip Planner</h1>
+      <h2>Select Start and End Stations</h2>
+    </section>
+  );
+}
+
+Planner();
