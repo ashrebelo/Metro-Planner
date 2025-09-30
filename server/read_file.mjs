4264d07678befca7a3c6da7bdda7883578db2251
ashre <ashrebelo21@gmail.com>
2025-09-30 17:07:23 -0400

    add try catch to reading file and JSON parse

--- server/read_file.mjs
+++ server/read_file.mjs
@@ -7,9 +7,15 @@ let stmData = [];
 
 export async function readGeoJSON() {
   if(stmData.length === 0) {
-    const content = await fs.readFile(filePath, 'utf8');
-    const geoData = await JSON.parse(content);
-    stmData =  geoData.features;
+    try {
+      const filePath = path.join('./data', fileName);
+      const content = await fs.readFile(filePath, 'utf8');
+      const geoData = await JSON.parse(content);
+      stmData =  geoData.features || geoData;
+    }catch(error) {
+      console.error('Failed File Reading', error);
+      stmData = [];
+    }
   }
   return stmData;
 }
