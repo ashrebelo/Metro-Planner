d8ba1d8cc1b22e511142b676dadac6c63a9788ac
ashre <ashrebelo21@gmail.com>
2025-09-30 18:38:25 -0400

    add stations and styling

---
+++ metro-client/src/components/Stations.jsx
@@ -0,0 +1,14 @@
+import Station from './Station';
+import './Stations.css';
+
+function Stations({stations, color}) {
+  return (
+    <section id="stations">
+      {stations.map((stat, index) =>
+        <Station station={stat} color={color} key={index} />
+      )}
+    </section>
+  );
+}
+
+export default Stations;
