199d4c324521cdf08ede0ab974a49f2b02ed601e
ashre <ashrebelo21@gmail.com>
2025-09-30 18:37:33 -0400

    add list of stations for testing

--- metro-client/src/App.jsx
+++ metro-client/src/App.jsx
@@ -1,6 +1,15 @@
 //import { useState } from "react";
 import './App.css';
 import MapExample from './components/MapExample';
+import Planner from './components/Planner';
+import Stations from './components/Stations';
+
+const stations = [
+  {'stop_name' : 'Station Angrinon', 'route_id' : '1'},
+  {'stop_name' : 'Station Monk', 'route_id' : '1'}
+];
+
+const colors = ['green', 'orange', 'null', 'yellow', 'blue'];
 
 function App() {
   const routeId = parseInt(stations[0].route_id, 10);
