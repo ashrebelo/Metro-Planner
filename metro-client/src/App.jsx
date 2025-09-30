ebe889e729f1b9f72724caeb870745c79dfa5542
ashre <ashrebelo21@gmail.com>
2025-09-30 18:36:57 -0400

    add color for lines

--- metro-client/src/App.jsx
+++ metro-client/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 import MapExample from './components/MapExample';
 
 function App() {
+  const routeId = parseInt(stations[0].route_id, 10);
+  const color = colors[routeId - 1];
   return (
     <div className="App">
       <MapExample />
