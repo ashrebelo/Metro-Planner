a8a80e0693f364018e1be2993b937af9f05a2bcf
ashre <ashrebelo21@gmail.com>
2025-10-07 14:24:59 -0400

    initial commit

---
+++ metro-client/src/components/EndPlanner.jsx
@@ -0,0 +1,28 @@
+import './Planner.css';
+
+function EndPlanner({endStation, setEndStation, routeStation}) {
+
+  function handleEndStation(event) {
+    setEndStation(event.target.value);
+  }
+
+  return (
+    <section id="trip-planner">
+      <label htmlFor="end-stations">End Station:</label>
+      <select 
+        name="end-stations"
+        value={endStation || ''}
+        onChange={handleEndStation}>
+        <option>-- Select End --</option>
+        {routeStation.map((station) => {
+          return <option key={`${station.route_id}-${station.id}-${station.stop_name}`} 
+            value={station.stop_name}>
+            {station.stop_name}
+          </option>;
+        })}
+      </select>
+    </section>
+  );
+}
+
+export default EndPlanner;
