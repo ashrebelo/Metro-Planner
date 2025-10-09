4ddc66fe4df3f56f514a4a8ed74266c76a5f602d
ashre <ashrebelo21@gmail.com>
2025-10-08 21:32:00 -0400

    remove console.log

--- metro-client/src/components/MapExample.jsx
+++ metro-client/src/components/MapExample.jsx
@@ -49,7 +49,6 @@ export default function MapExample({routeTrip, color}) {
           attribution={attribution}
           url={tileUrl}
         />
-        {console.log(routeTrip.length)}
         {routeTrip.length > 1 && <MetroMarkers route={points} routeTrip={routeTrip} color={color}/>}
       </MapContainer>
     </div>
