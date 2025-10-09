df61858da679f6c096e7b5aae15f1fe86255e031
ashre <ashrebelo21@gmail.com>
2025-10-08 21:36:48 -0400

    create function to get wiki api

--- metro-client/src/components/MetroMarkers.jsx
+++ metro-client/src/components/MetroMarkers.jsx
@@ -22,7 +22,7 @@ export default function MetroMarkers({routeTrip, color}) {
         const p = [routeTrip[index].coordinates[1], routeTrip[index].coordinates[0]];
         points.push(p);
         return <Marker key={index} position={p} icon={customIcon}>
-          <Popup>{handleStationInfo}</Popup>
+          <Popup>{point.stop_name}</Popup>
         </Marker>;
       })}
       <Polyline pathOptions={{color: color}} positions={points} />
