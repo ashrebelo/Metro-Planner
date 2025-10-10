843285fd31ef416b4556b73ca0de50bf6cb0fa6d
ashre <ashrebelo21@gmail.com>
2025-10-10 17:12:12 -0400

    add comment

--- metro-client/src/components/Station.jsx
+++ metro-client/src/components/Station.jsx
@@ -4,6 +4,8 @@ import './Stations.css';
  * Create a block that makes a box using styling and display stations name
  * @param station refers to a single station
  * @param color the color of the line the station is on
+ * @param selectClass refer to the class name given to blocks of station that are select
+ * otherwise if is ''
  * @returns a section with a block section and station name
  */
 function Station({station, color, selectClass}) {
