cf02f627a9c0d322444f92616133e068566afb4b
ashre <ashrebelo21@gmail.com>
2025-10-10 17:14:14 -0400

    add selectedStation as a param

--- metro-client/src/components/Stations.jsx
+++ metro-client/src/components/Stations.jsx
@@ -5,9 +5,10 @@ import './Stations.css';
  * Loops through the routeTrip to call Station component
  * @param routeTrip list of stations from start to end
  * @param color color of the line
+ * @param selectedStation refers to the station that is selected for more info
  * @returns Station component
  */
-function Stations({routeTrip, color}) {
+function Stations({routeTrip, color, selectedStation}) {
   return (
     <section id="stations">
       {routeTrip.map((stat, index) =>
