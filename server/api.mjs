b43cbcc50a9e748212b2bc85dfd9426aac467182
ashre <ashrebelo21@gmail.com>
2025-11-13 10:03:11 -0500

    change to return status

--- server/api.mjs
+++ server/api.mjs
@@ -58,7 +58,7 @@ app.get('/api/routetrip/:startStationId/:endStationId', (req, res) => {
  * https://expressjs.com/en/starter/static-files.html
  */
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../metro-client/dist/index.html'));
+  return res.sendFile(path.join(__dirname, '../metro-client/dist/index.html'));
 });
 
 /**
